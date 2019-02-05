042f262b95d8ded80c4b56dbd7fb4fa0d7035f67
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-05 18:27:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -51,7 +51,9 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if(cardNumber){
-      return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}`;
+      const result = `Your total cost is ${total()}, which will be charged to the card ${cardNumber}`;
+      cart = [];
+      return result;
   } else {
     return "Sorry, we don't have a credit card on file for you.";
   }

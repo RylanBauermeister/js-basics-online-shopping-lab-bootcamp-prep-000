75ad4ef86a5aeb0f8f469256ac42cc5c76b4cc54
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-05 18:05:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
  var objItem = {
-   itemName: name,
+   itemName: item,
    itemPrice: Math.floor(Math.random()*100)
  };
  cart.push(objItem);
